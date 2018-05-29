d1fa31fad7d898daf60cf7bd49d411f02f034e18
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-05-29 08:23:02 -0700

    coded the apis

--- js/script.js
+++ js/script.js
@@ -12,8 +12,46 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var $street = $("#street").val();
+    var $city   = $("#city").val();
+    var streetViewImage;
 
-    // YOUR CODE GOES HERE!
+    if ($street.length > 0 && $city.length > 0) {
+        apiUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=';
+        apiUrl += $street
+        apiUrl += ', '
+        apiUrl += $city
+        streetViewImage =apiUrl.replace(/ /gi, '%20');
+        console.log(streetViewImage)
+        var img = `<img class="bgimg" src="${streetViewImage}">`
+        $body.append(img);
+    }
+
+    // NYTimes
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "f7d60cc6c9ec4779987719ebe7782a10",
+        'q': "geary st, san francisco"
+      });
+    
+    var NYarticles = []
+
+    $.ajax({
+    url: url,
+    method: 'GET',
+    }).done(function(result) {
+    console.log(result.response.docs);
+    result.response.docs.forEach((doc) => {
+        NYarticles.push({'headline': doc.headline.main, 'body': doc.snippet});
+    });
+
+    NYarticles.forEach((article) => {
+        $nytElem.append(`<li>${article.headline}</li>`)
+    })
+    console.log(NYarticles)
+    }).fail(function(err) {
+    throw err;
+    });
 
     return false;
 };
